790bf18188c5eb1a2537001538bb4aa8ccaeb2ff
Stefano Chiesa <base278@gmail.com>
2015-08-10 20:38:52 +0800

    Add answer collection and checking script

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function newTextNode(text) {
   return document.createTextNode(text);
 }
 
-function addOption(event) {
-  var parent = event.parentNode;
+function addOption(target) {
+  var parent = target.parentNode;
 
   var questionNumber = parent.getElementsByTagName("input")[0].getAttribute("name");
   var questionOptionName = questionNumber+"-option";
@@ -25,29 +25,29 @@ function addOption(event) {
   newOptionNode.setAttribute("type", "radio");
   newOptionNode.setAttribute("name", questionOptionName);
   newOptionNode.setAttribute("value", newOptionName);
-  console.log(newOptionNode);
+  if (currentNumberOfOption === 0) {
+    newOptionNode.setAttribute("checked", "");
+  }
 
   var newOptionTextNode = document.createElement("input");
   newOptionTextNode.setAttribute("type", "text");
   newOptionTextNode.setAttribute("name", newOptionName+"-text");
   newOptionTextNode.setAttribute("placeholder", "Type option "+newNumberOfOption+"!");
   newOptionTextNode.setAttribute("class", "option-text");
-  console.log(newOptionTextNode);
 
 
-  parent.insertBefore(newOptionNode, event);
-  parent.insertBefore(newTextNode(" "), event);
-  parent.insertBefore(newOptionTextNode, event);
-  parent.insertBefore(newLineNode(), event);
+  parent.insertBefore(newOptionNode, target);
+  parent.insertBefore(newTextNode(" "), target);
+  parent.insertBefore(newOptionTextNode, target);
+  parent.insertBefore(newLineNode(), target);
 
 }
 
-function addQuestion(event) {
-  var parent = event.parentNode.parentNode.parentNode;
-  var parentChild = event.parentNode.parentNode;
+function addQuestion(target) {
+  var parent = target.parentNode.parentNode.parentNode;
+  var parentChild = target.parentNode.parentNode;
 
   var currentNumberOfQuestion = parent.getElementsByTagName("tr").length-1;
-  console.log(parent);
 
   var newNumberOfQuestion = currentNumberOfQuestion+1;
   var questionName = "question-"+newNumberOfQuestion;
@@ -80,11 +80,92 @@ function addQuestion(event) {
 
   newRow.appendChild(newQuestionNode);
 
-  console.log(newRow);
-
   parent.insertBefore(newRow, parentChild);
 
   addOption(newAddOptionNode);
 
 
+}
+
+var answerKey;
+
+function collectQuestions() {
+  var inputData = document.getElementsByTagName("input");
+  var correctData = [];
+  for (var index = 0; index < inputData.length; index++) {
+    node = inputData[index];
+    if (node.getAttribute("type") === "radio") {
+      if (node.checked) {
+        correctData.push(node.getAttribute("value"));
+      }
+    }
+  }
+  return correctData;
+}
+
+function transformInput() {
+  var inputData = document.getElementsByTagName("input");
+  for (var index = inputData.length-1; index >= 0; index--) {
+    var node = inputData[index];
+    if (node.getAttribute("type") === "submit") {
+      node.setAttribute("onclick", "checkAnswer(event)");
+    }
+    else if (node.getAttribute("type") === "radio") {
+      if (!!node.checked) {
+        node.checked = false;
+      }
+    }
+    else {
+      parent = node.parentNode;
+      parent.replaceChild(newTextNode(node.value), node);
+    }
+  }
+}
+
+function removeAddOptionAndQuestion() {
+  var linkElements = document.getElementsByTagName("a");
+  for(var index = linkElements.length-1; index >= 0; index--) {
+    var node = linkElements[index];
+    node.parentNode.removeChild(node);
+  }
+}
+
+function generateQuiz(event) {
+  event.preventDefault();
+  answerKey = collectQuestions();
+  transformInput();
+  removeAddOptionAndQuestion();
+}
+
+function checkAnswer(event) {
+  console.log(answerKey);
+  event.preventDefault();
+  var inputData = Array.prototype.slice.call(document.getElementsByTagName("input"));
+  var correctCount = 0;
+  var submitNode;
+  for (var index = 0; index < inputData.length; index++) {
+    node = inputData[index];
+    if (node.getAttribute("type") === "radio") {
+      if (!!node.checked) {
+        if (answerKey.indexOf(node.getAttribute("value")) >= 0) {
+          correctCount++;
+          parent = node.parentNode;
+          parent.replaceChild(newTextNode("\u2714"), node);
+        }
+        else {
+          parent = node.parentNode;
+          parent.replaceChild(newTextNode("\u2716"), node);
+        }
+      }
+      else {
+        parent = node.parentNode;
+        parent.replaceChild(newTextNode("\u2750"), node);
+      }
+    }
+    else if (node.getAttribute("type") === "submit") {
+      submitNode = node;
+    }
+  }
+  parent = submitNode.parentNode;
+  parent.replaceChild(newTextNode("You got "+correctCount+" answer correct!"), submitNode);
 }
